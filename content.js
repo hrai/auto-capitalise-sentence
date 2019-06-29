caf75a0bfd2f01067a783d3da84cdf3e75f97b18
Hangjit Rai <rai.hangjit@gmail.com>
2019-06-29 13:46:36 +1000

    Added Logic To Change Case

--- content.js
+++ content.js
@@ -1,5 +1,19 @@
  $(document).ready(function(){
-    $(":text").keyup(function(e){
-        console.log(e);
+     //change to text area
+    $(":text").keyup(function(event){
+        var textBox =event.target;
+        var text = $(textBox).val();
+
+        var regex =/^\w+\.\s*\w/;
+        var matches = regex.test(text);
+
+        console.log(matches);
+
+        var lastChar = text.slice(-1);
+        var updatedStr = text.substr(0, text.length-1) + lastChar.toUpperCase();
+
+        textBox.value = (updatedStr);
+        console.log(event);
+
     });
 });
