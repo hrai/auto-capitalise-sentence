8886a2f486c8b146c2e1ad72ee697737c130c0b8
Hangjit Rai <rai.hangjit@gmail.com>
2019-06-29 14:47:31 +1000

    Cleanup

--- content.js
+++ content.js
@@ -3,15 +3,19 @@ $(document).ready(function(){
         var textBox =event.target;
         var text = $(textBox).val();
 
-        var regex =/\w+\s*\.+\s*\w$/;
+        if(text.length == 1) {
+            textBox.value = text.toUpperCase();
+            return;
+        }
+
+        var regex =/\w+\.+\s*\w$/;
         var matches = regex.test(text);
 
         if(matches) {
-
             var lastChar = text.slice(-1);
             var updatedStr = text.substr(0, text.length-1) + lastChar.toUpperCase();
 
-            textBox.value = (updatedStr);
+            textBox.value = updatedStr;
         }
 
         // console.log(event);
