7e5a2edf15e6c1fea51eafc440e88be69cfa7278
Hangjit Rai <rai.hangjit@gmail.com>
2019-06-29 13:52:51 +1000

    Checking Whitespaces

--- content.js
+++ content.js
@@ -1,18 +1,21 @@
- $(document).ready(function(){
-     //change to text area
-    $(":text").keyup(function(event){
+$(document).ready(function(){
+    //change to text area
+    $("textarea").keyup(function(event){
         var textBox =event.target;
         var text = $(textBox).val();
 
-        var regex =/^\w+\.\s*\w/;
+        var regex =/^\w+\s*\.+\s*\w/;
         var matches = regex.test(text);
 
         console.log(matches);
 
-        var lastChar = text.slice(-1);
-        var updatedStr = text.substr(0, text.length-1) + lastChar.toUpperCase();
+        if(matches) {
 
-        textBox.value = (updatedStr);
+            var lastChar = text.slice(-1);
+            var updatedStr = text.substr(0, text.length-1) + lastChar.toUpperCase();
+
+            textBox.value = (updatedStr);
+        }
         console.log(event);
 
     });
