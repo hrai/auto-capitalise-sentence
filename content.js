60966bbab1d2f536ee77ba64aa902355a7377190
Hangjit Rai <rai.hangjit@gmail.com>
2019-07-03 21:15:10 +1000

    Setting Up Code To Disable Loading In Selected Sites

--- content.js
+++ content.js
@@ -1,23 +1,56 @@
 $(document).ready(function(){
-    $(":text,textarea").keydown(function(event){
-        var textBox =event.target;
-        var text = $(textBox).val();
+    var sitesToExclude = [];
 
-        if(text.length == 1) {
-            textBox.value = text.toUpperCase();
-            return;
+    function excludeSite(site) {
+        sitesToExclude.push(site);
+    }
+
+    //https://stackoverflow.com/questions/406192/get-current-url-with-jquery
+    var currentUrlDomain = window.location.origin;
+
+    var totalSites =sitesToExclude.length;
+    if(totalSites ==0) {
+        hookupEventHandlers();
+        return;
+    }
+
+    if(totalSites !=0) {
+        try {
+            sitesToExclude.forEach(function(siteToExclude) {
+                if(!siteToExclude.includes(currentUrlDomain)) {
+                    hookupEventHandlers();
+                    throw BreakException;
+                }
+            });
         }
-
-        var regex =/\w+\.+\s+\w$/;
-        var matches = regex.test(text);
-
-        if(matches) {
-            var lastChar = text.slice(-1);
-            var updatedStr = text.substr(0, text.length-1) + lastChar.toUpperCase();
-
-            textBox.value = updatedStr;
+        catch (e) {
+            if (e !== BreakException) {
+                throw e;
+            }
         }
+    }
 
-        // console.log(event);
-    });
+    function hookupEventHandlers() {
+        $(":text,textarea").keydown(function(event){
+            var textBox =event.target;
+            var text = $(textBox).val();
+
+            if(text.length == 1) {
+                textBox.value = text.toUpperCase();
+                return;
+            }
+
+            var regex =/\w+\.+\s+\w$/;
+            var matches = regex.test(text);
+
+            if(matches) {
+                var lastChar = text.slice(-1);
+                var updatedStr = text.substr(0, text.length-1) + lastChar.toUpperCase();
+
+                textBox.value = updatedStr;
+            }
+
+            // console.log(event);
+        });
+    }
 });
