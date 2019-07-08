99809c364a86fe5ec8951b648875fb3d8318375c
Hangjit Rai <rai.hangjit@gmail.com>
2019-07-08 14:35:36 +1000

    Modified Popup Html

--- popup/settings.js
+++ popup/settings.js
@@ -2,11 +2,13 @@
 $(document).ready(function(){
     var sitesToExclude = [];
 
- document
-        .getElementById("submitButton")
-        .addEventListener("click", function() {
-            alert('test');
-        });
+    console.log('test');
+
+//  document
+//         .getElementById('submitButton').innerHTML='Test';
+//         // .addEventListener("click", function() {
+//         //     alert('test');
+//         // });
     $('#submitButton').click(function() {
         var sitesBox = $('#sites');
         console.log(sitesBox.val());
