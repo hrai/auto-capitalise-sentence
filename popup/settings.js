032ccf2ac2e0b3346594e8a740107ef9ccc206d7
Hangjit Rai <rai.hangjit@gmail.com>
2019-07-08 14:35:49 +1000

    Cleanup

--- popup/settings.js
+++ popup/settings.js
@@ -4,11 +4,6 @@ $(document).ready(function(){
 
     console.log('test');
 
-//  document
-//         .getElementById('submitButton').innerHTML='Test';
-//         // .addEventListener("click", function() {
-//         //     alert('test');
-//         // });
     $('#submitButton').click(function() {
         var sitesBox = $('#sites');
         console.log(sitesBox.val());
