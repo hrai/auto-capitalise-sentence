84e1bf71efe1ce0f08687c7d27918d97cea28f74
Hangjit Rai <rai.hangjit@gmail.com>
2019-07-14 12:05:09 +1000

    Cleanup

--- content.js
+++ content.js
@@ -1,157 +1,157 @@
 $(document).ready(function(){
-  var sitesToExclude = [];
-  var elementsWithModifiedContents = [];
+    var sitesToExclude = [];
+    var elementsWithModifiedContents = [];
 
-  function excludeSite(site) {
-    sitesToExclude.push(site);
-  }
+    function excludeSite(site) {
+        sitesToExclude.push(site);
+    }
 
-  //https://stackoverflow.com/questions/406192/get-current-url-with-jquery
-  var currentUrlDomain = window.location.origin;
+    //https://stackoverflow.com/questions/406192/get-current-url-with-jquery
+    var currentUrlDomain = window.location.origin;
 
-  var totalSites =sitesToExclude.length;
-  if(totalSites ==0) {
-    hookupEventHandler();
+    var totalSites =sitesToExclude.length;
+    if(totalSites ==0) {
+        hookupEventHandler();
 
-    return;
-  }
+        return;
+    }
 
-  if(totalSites !=0) {
-    try {
-      sitesToExclude.forEach(function(siteToExclude) {
-        if(!siteToExclude.includes(currentUrlDomain)) {
-          hookupEventHandler();
+    if(totalSites !=0) {
+        try {
+            sitesToExclude.forEach(function(siteToExclude) {
+                if(!siteToExclude.includes(currentUrlDomain)) {
+                    hookupEventHandler();
 
-          throw BreakException;
+                    throw BreakException;
+                }
+            });
         }
-      });
-    }
-    catch (e) {
-      if (e !== BreakException) {
-        throw e;
-      }
-    }
-  }
-
-  function setText(htmlControl, tagName, updatedStr) {
-    if(tagName.toUpperCase()==='INPUT' || tagName.toUpperCase()==='TEXTAREA') {
-      htmlControl.val(updatedStr);
-      return;
+        catch (e) {
+            if (e !== BreakException) {
+                throw e;
+            }
+        }
     }
 
-    htmlControl.html(updatedStr);
+    function setText(htmlControl, tagName, updatedStr) {
+        if(tagName.toUpperCase()==='INPUT' || tagName.toUpperCase()==='TEXTAREA') {
+            htmlControl.val(updatedStr);
+            return;
+        }
 
-    elementsWithModifiedContents.push(htmlControl.html());
-  }
+        htmlControl.html(updatedStr);
 
-  function getText(htmlControl, tagName) {
-    if(tagName.toUpperCase()==='INPUT' || tagName.toUpperCase()==='TEXTAREA') {
-      return htmlControl.val();
+        elementsWithModifiedContents.push(htmlControl.html());
     }
 
-    return htmlControl.html();
-  }
+    function getText(htmlControl, tagName) {
+        if(tagName.toUpperCase()==='INPUT' || tagName.toUpperCase()==='TEXTAREA') {
+            return htmlControl.val();
+        }
 
-  function capitaliseTextForInputTags(element) {
-    var htmlControl = $(element);
-
-    var tagName = htmlControl.prop('tagName');
-    var text = getText(htmlControl, tagName);
-
-    if(text.length == 1) {
-      setText(htmlControl, tagName, text.toUpperCase())
-      return;
+        return htmlControl.html();
     }
 
-    var regex =/\w+\s*(\.|\?)+\s+\w$/;
-    var matches = regex.test(text);
+    function capitaliseTextForInputTags(element) {
+        var htmlControl = $(element);
 
-    if(matches) {
-      var lastChar = text.slice(-1);
-      var updatedStr = text.substr(0, text.length-1) + lastChar.toUpperCase();
+        var tagName = htmlControl.prop('tagName');
+        var text = getText(htmlControl, tagName);
 
-      setText(htmlControl, tagName, updatedStr)
+        if(text.length == 1) {
+            setText(htmlControl, tagName, text.toUpperCase())
+            return;
+        }
+
+        var regex =/\w+\s*(\.|\?)+\s+\w$/;
+        var matches = regex.test(text);
+
+        if(matches) {
+            var lastChar = text.slice(-1);
+            var updatedStr = text.substr(0, text.length-1) + lastChar.toUpperCase();
+
+            setText(htmlControl, tagName, updatedStr)
+        }
+
+        // console.log(event);
     }
 
-    // console.log(event);
-  }
+    function capitaliseText(mutation) {
 
-  function capitaliseText(mutation) {
+        var htmlControl = $(mutation.parent());
 
-    var htmlControl = $(mutation.parent());
-
-    var tagName = htmlControl.prop('tagName');
-    var text=mutation.text();
-    if(elementsWithModifiedContents.indexOf(text) >= 0)
-      return;
+        var tagName = htmlControl.prop('tagName');
+        var text=mutation.text();
+        if(elementsWithModifiedContents.indexOf(text) >= 0)
+            return;
 
 
-    if(text.length == 1) {
-      setText(htmlControl, tagName, text.toUpperCase())
-      return;
+        if(text.length == 1) {
+            setText(htmlControl, tagName, text.toUpperCase())
+            return;
+        }
+
+        var regex =/\w+\s*(\.|\?)+\s+\w$/;
+        var matches = regex.test(text);
+
+        if(matches) {
+            var lastChar = text.slice(-1);
+            var updatedStr = text.substr(0, text.length-1) + lastChar.toUpperCase();
+
+            setText(htmlControl, tagName, updatedStr)
+        }
+
+        // console.log(event);
     }
 
-    var regex =/\w+\s*(\.|\?)+\s+\w$/;
-    var matches = regex.test(text);
-
-    if(matches) {
-      var lastChar = text.slice(-1);
-      var updatedStr = text.substr(0, text.length-1) + lastChar.toUpperCase();
-
-      setText(htmlControl, tagName, updatedStr)
-    }
-
-    // console.log(event);
-  }
-
-  function hookupEventHandler() {
-    $(":text,textarea").keydown(function(event){
-      capitaliseTextForInputTags(event.target);
-    });
-
-    wireupPtagHandlers();
-  }
-
-  function hookupHtmlChangeEventHandler(element) {
-    var observer = new MutationObserver(function(mutations, observer) {
-      $.each(mutations, function (i, mutation) {
-        var target = $(mutation.target);
-
-        capitaliseText(target);
-      });
-    });
-
-    var config = {
-      childList: true,
-      attributes: true,
-      subtree: true,
-      characterData: true
-    };
-
-    observer.observe(element, config);
-  }
-
-  function wireupPtagHandlers() {
-    var target = document.querySelector("div");
-
-    var observer = new MutationObserver(function(mutations, observer) {
-      $.each(mutations, function (i, mutation) {
-        var addedNodes = $(mutation.addedNodes);
-        var selector = "p"
-        var filteredEls = addedNodes.find(selector).addBack(selector); // finds either added alone or as tree
-        filteredEls.each(function(index, element) {
-          hookupHtmlChangeEventHandler(element);
+    function hookupEventHandler() {
+        $(":text,textarea").keydown(function(event){
+            capitaliseTextForInputTags(event.target);
         });
-      });
-    });
+
+        wireupPtagHandlers();
+    }
+
+    function hookupHtmlChangeEventHandler(element) {
+        var observer = new MutationObserver(function(mutations, observer) {
+            $.each(mutations, function (i, mutation) {
+                var target = $(mutation.target);
+
+                capitaliseText(target);
+            });
+        });
+
+        var config = {
+            childList: true,
+            attributes: true,
+            subtree: true,
+            characterData: true
+        };
+
+        observer.observe(element, config);
+    }
+
+    function wireupPtagHandlers() {
+        var target = document.querySelector("div");
+
+        var observer = new MutationObserver(function(mutations, observer) {
+            $.each(mutations, function (i, mutation) {
+                var addedNodes = $(mutation.addedNodes);
+                var selector = "p"
+                var filteredEls = addedNodes.find(selector).addBack(selector); // finds either added alone or as tree
+                filteredEls.each(function(index, element) {
+                    hookupHtmlChangeEventHandler(element);
+                });
+            });
+        });
 
 
-    var config = {
-      childList: true,
-      subtree: true,
-      characterData: true
-    };
+        var config = {
+            childList: true,
+            subtree: true,
+            characterData: true
+        };
 
-    observer.observe(target, config);
-  }
+        observer.observe(target, config);
+    }
 });
