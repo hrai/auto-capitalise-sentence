6908025afc1b7870d3c2a10de77506ae625c5075
Hangjit Rai <rai.hangjit@gmail.com>
2019-07-23 22:14:15 +1000

    cleanup

--- popup/settings.js
+++ popup/settings.js
@@ -3,7 +3,10 @@ $(document).ready(function(){
     browser.storage.local.get('sites_to_ignore').then(processResponse, onError);
 
     function processResponse(item) {
-        $('#sites').val(item.sites_to_ignore);
+        var sitesToExclude =item.sites_to_ignore;
+        if(sitesToExclude) {
+            $('#sites').val(sitesToExclude.join('\n'));
+        }
     }
 
     function onError(error) {
@@ -17,12 +20,15 @@ $(document).ready(function(){
             {
                 'sites_to_ignore': sites
             });
+
+        $(this).prop('disabled', true);
+        $(this).val('Saved');
     });
 
     function getSites() {
         var sitesBoxVal = $('#sites').val();
 
-        if( sitesBoxVal) {
+        if(sitesBoxVal) {
             var sites = sitesBoxVal.split('\n');
             return sites;
         }
