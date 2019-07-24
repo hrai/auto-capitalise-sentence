379d2a3ab5b899e9eaa0eafde09a330b312ab1ae
Hangjit Rai <rai.hangjit@gmail.com>
2019-07-24 19:29:51 +1000

    PERF Only Enabling Capitilising On Content Editable Elements

--- content.js
+++ content.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+$(document).ready(function() {
     // function excludeSite(site) {
     //     sitesToExclude.push(site);
     // }
@@ -11,35 +11,33 @@ $(document).ready(function(){
     }
 
     function processResponse(item) {
-        var sitesToExclude=item.sites_to_ignore;
+        var sitesToExclude = item.sites_to_ignore;
 
-        if(item && sitesToExclude) {
+        if (item && sitesToExclude) {
             //https://stackoverflow.com/questions/406192/get-current-url-with-jquery
             var currentUrlDomain = window.location.origin;
-            var errorMsg ='breaking loop';
+            var errorMsg = 'breaking loop';
 
             try {
                 var shouldEnableCapitalisingOnCurrentSite = true;
 
-                $.each(sitesToExclude, function (i, siteToExclude) {
-                    if(currentUrlDomain.includes(siteToExclude)) {
-                        shouldEnableCapitalisingOnCurrentSite=false;
+                $.each(sitesToExclude, function(i, siteToExclude) {
+                    if (currentUrlDomain.includes(siteToExclude)) {
+                        shouldEnableCapitalisingOnCurrentSite = false;
                     }
                 });
 
-                if(shouldEnableCapitalisingOnCurrentSite) {
+                if (shouldEnableCapitalisingOnCurrentSite) {
                     hookupEventHandlers();
 
                     throw new Error(errorMsg);
                 }
-            }
-            catch (e) {
+            } catch (e) {
                 if (e.message !== errorMsg) {
                     throw e;
                 }
             }
-        }
-        else {
+        } else {
             hookupEventHandlers();
         }
     }
@@ -49,7 +47,10 @@ $(document).ready(function(){
     }
 
     function getText(htmlControl, tagName) {
-        if(tagName.toUpperCase()==='INPUT' || tagName.toUpperCase()==='TEXTAREA') {
+        if (
+            tagName.toUpperCase() === 'INPUT' ||
+      tagName.toUpperCase() === 'TEXTAREA'
+        ) {
             return htmlControl.val();
         }
 
@@ -57,7 +58,10 @@ $(document).ready(function(){
     }
 
     function setText(htmlControl, tagName, updatedStr) {
-        if(tagName.toUpperCase()==='INPUT' || tagName.toUpperCase()==='TEXTAREA') {
+        if (
+            tagName.toUpperCase() === 'INPUT' ||
+      tagName.toUpperCase() === 'TEXTAREA'
+        ) {
             htmlControl.val(updatedStr);
             return;
         }
@@ -66,36 +70,34 @@ $(document).ready(function(){
         setEndOfContenteditable(htmlControl[0]);
     }
 
-    function setEndOfContenteditable(contentEditableElement)
-    {
-        var range,selection;
-        if(document.createRange)//Firefox, Chrome, Opera, Safari, IE 9+
-        {
-            range = document.createRange();//Create a range (a range is a like the selection but invisible)
-            range.selectNodeContents(contentEditableElement);//Select the entire contents of the element with the range
-            range.collapse(false);//collapse the range to the end point. false means collapse to end rather than the start
-            selection = window.getSelection();//get the selection object (allows you to change selection)
-            selection.removeAllRanges();//remove any selections already made
-            selection.addRange(range);//make the range you have just created the visible selection
-        }
-        else if(document.selection)//IE 8 and lower
-        {
-            range = document.body.createTextRange();//Create a range (a range is a like the selection but invisible)
-            range.moveToElementText(contentEditableElement);//Select the entire contents of the element with the range
-            range.collapse(false);//collapse the range to the end point. false means collapse to end rather than the start
-            range.select();//Select the range (make it the visible selection
+    function setEndOfContenteditable(contentEditableElement) {
+        var range, selection;
+        if (document.createRange) {
+            //Firefox, Chrome, Opera, Safari, IE 9+
+            range = document.createRange(); //Create a range (a range is a like the selection but invisible)
+            range.selectNodeContents(contentEditableElement); //Select the entire contents of the element with the range
+            range.collapse(false); //collapse the range to the end point. false means collapse to end rather than the start
+            selection = window.getSelection(); //get the selection object (allows you to change selection)
+            selection.removeAllRanges(); //remove any selections already made
+            selection.addRange(range); //make the range you have just created the visible selection
+        } else if (document.selection) {
+            //IE 8 and lower
+            range = document.body.createTextRange(); //Create a range (a range is a like the selection but invisible)
+            range.moveToElementText(contentEditableElement); //Select the entire contents of the element with the range
+            range.collapse(false); //collapse the range to the end point. false means collapse to end rather than the start
+            range.select(); //Select the range (make it the visible selection
         }
     }
 
     function shouldCapitalise(text) {
-        var regex =/\w+\s*(\.|\?)+\s+\w$/;
+        var regex = /\w+\s*(\.|\?)+\s+\w$/;
         var matches = regex.test(text);
 
         return matches;
     }
 
     function shouldCapitaliseForI(text) {
-        var regex =/\s+i(\s+|')$/;
+        var regex = /\s+i(\s+|')$/;
         var matches = regex.test(text);
 
         return matches;
@@ -107,19 +109,19 @@ $(document).ready(function(){
         var tagName = htmlControl.prop('tagName');
         var text = getText(htmlControl, tagName);
 
-        if(text.length == 1) {
+        if (text.length == 1) {
             setText(htmlControl, tagName, text.toUpperCase());
             return;
         }
 
-        if(shouldCapitalise(text)) {
+        if (shouldCapitalise(text)) {
             var updatedStr = getCapitalisedContent(text);
 
             setText(htmlControl, tagName, updatedStr);
             return;
         }
 
-        if(text.length >= 2 && shouldCapitaliseForI(text)) {
+        if (text.length >= 2 && shouldCapitaliseForI(text)) {
             var updatedStr = getCapitalisedContentForI(text);
 
             setText(htmlControl, tagName, updatedStr);
@@ -129,18 +131,19 @@ $(document).ready(function(){
 
     function getCapitalisedContentForI(text) {
         var lastTwoChars = text.slice(-2);
-        var updatedStr = text.substr(0, text.length-2) + lastTwoChars.toUpperCase();
+        var updatedStr =
+      text.substr(0, text.length - 2) + lastTwoChars.toUpperCase();
         return updatedStr;
     }
 
     function getCapitalisedContent(text) {
         var lastChar = text.slice(-1);
-        var updatedStr = text.substr(0, text.length-1) + lastChar.toUpperCase();
+        var updatedStr = text.substr(0, text.length - 1) + lastChar.toUpperCase();
         return updatedStr;
     }
 
     function wireupInputTagHandlers() {
-        $(':text,textarea').on('input', function(event){
+        $(':text,textarea').on('input', function(event) {
             capitaliseText(event.target);
         });
     }
@@ -148,8 +151,7 @@ $(document).ready(function(){
     function containsHtmlContent(element) {
         var content = $(element).html();
 
-        if(content && content === '<br>')
-            return false;
+        if (content && content === '<br>') return false;
 
         var regex = /<\/?\w+>/;
         var hasHtmlTag = regex.test(content);
@@ -157,25 +159,26 @@ $(document).ready(function(){
     }
 
     function wireupTextChangeHandler(element) {
-
-        if(!containsHtmlContent(element)) {
-            if($(element).html()) {
+        if (!containsHtmlContent(element)) {
+            if ($(element).html()) {
                 capitaliseText(element);
             }
 
             var observer = new MutationObserver(function(mutations) {
                 var processed = false;
-                $.each(mutations, function (i, mutation) {
-                    if(!processed) {
+                $.each(mutations, function(i, mutation) {
+                    if (!processed) {
                         var target = $(mutation.target);
 
                         var tagName = target.prop('tagName');
-                        if(!tagName) {
+                        if (!tagName) {
                             target = $(target).parent();
                         }
 
-                        capitaliseText(target);
-                        processed = true;
+                        if (isContentEditable(target)) {
+                            capitaliseText(target);
+                            processed = true;
+                        }
                     }
                 });
             });
@@ -190,34 +193,39 @@ $(document).ready(function(){
         }
     }
 
+    function isContentEditable(element) {
+        return element && element.isContentEditable;
+    }
+
     function wireupHtmlTagsAddedHandlers() {
         var target = document.querySelector('body');
 
-        var tags = ['p','span'];
+        var tags = ['p', 'span'];
         var inputTags = ['input[type=\'text\']', 'textarea'];
 
         var observer = new MutationObserver(function(mutations) {
-            $.each(mutations, function (i, mutation) {
+            $.each(mutations, function(i, mutation) {
                 var addedNodes = $(mutation.addedNodes);
 
                 try {
-                    $.each(tags, function(i,tagName) {
+                    $.each(tags, function(i, tagName) {
                         var filteredEls = addedNodes.find(tagName).addBack(tagName); // finds either added alone or as tree
                         filteredEls.each(function(index, element) {
                             wireupTextChangeHandler(element);
                         });
                     });
 
-                    $.each(inputTags, function(i,tagName) {
+                    $.each(inputTags, function(i, tagName) {
                         var filteredEls = addedNodes.find(tagName).addBack(tagName); // finds either added alone or as tree
                         filteredEls.each(function(index, element) {
-                            $(element).on('input', function(event){
-                                capitaliseText(event.target);
-                            });
+                            if (isContentEditable(element)) {
+                                $(element).on('input', function(event) {
+                                    capitaliseText(event.target);
+                                });
+                            }
                         });
                     });
-                }
-                catch(err) {
+                } catch (err) {
                     console.log(err);
                 }
             });
@@ -225,7 +233,7 @@ $(document).ready(function(){
 
         var config = {
             subtree: true,
-            childList: true,
+            childList: true
         };
 
         observer.observe(target, config);
