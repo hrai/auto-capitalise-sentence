89285aef34956a5d6bc00af5588af15e70a79c96
Hangjit Rai <rai.hangjit@gmail.com>
2019-08-08 22:11:44 +1000

    Updated Zipping Script

--- file_zipper.sh
+++ file_zipper.sh
@@ -1,4 +1,10 @@
 PUBLISHED_FILE=publish.zip
+
+if [ -f "$PUBLISHED_FILE" ]; then
+    echo "Deleting $PUBLISHED_FILE..."
+    rm -f $PUBLISHED_FILE
+fi
+
 zip -r9 $PUBLISHED_FILE dependencies icons popup background.js content.js LICENSE manifest.json
 
 echo "Successfully zipped to '$PUBLISHED_FILE'"
