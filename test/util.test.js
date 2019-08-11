d61c467c440929f11e5ec7e76dd1b7ce4ba0784b
Hangjit Rai <rai.hangjit@gmail.com>
2019-08-11 20:29:12 +1000

    ES6 CRE-ES6 CRE-ES6 Updated Tests

--- test/util.test.js
+++ test/util.test.js
@@ -1,5 +1,8 @@
 import * as utils from '../src/utilities.js';
 
-test('adds 1 + 2 to equal 3', () => {
-    expect(utils.getCapitalisedContent('blah')).toBe(3);
+test('getCapitalisedContent', () => {
+    expect(utils.getCapitalisedContent('blah')).toBe('blaH');
+    expect(utils.getCapitalisedContent('i')).toBe('I');
+    expect(utils.getCapitalisedContent('i\'m')).toBe('i\'M');
+    expect(utils.getCapitalisedContent('i. m')).toBe('i. M');
 });
