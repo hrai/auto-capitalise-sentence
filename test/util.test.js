b999d3beef3c48c8219730a14955ec2f5906ebc6
Hangjit Rai <rai.hangjit@gmail.com>
2019-08-11 21:11:22 +1000

    ES6 Cleanup

--- test/util.test.js
+++ test/util.test.js
@@ -1,61 +1,63 @@
 import * as utils from '../src/utilities.js';
 
-test('getCapitalisedContent', () => {
-    expect(utils.getCapitalisedContent('blah')).toBe('blaH');
-    expect(utils.getCapitalisedContent('i')).toBe('I');
-    expect(utils.getCapitalisedContent('i\'m')).toBe('i\'M');
-    expect(utils.getCapitalisedContent('i. m')).toBe('i. M');
-    expect(utils.getCapitalisedContent('')).toBe('');
-    expect(() => {
-        utils.getCapitalisedContent();
-    }).toThrow();
-});
-
-test('shouldCapitaliseForI', () => {
-    expect(utils.shouldCapitaliseForI('war i ')).toBe(true);
-    expect(utils.shouldCapitaliseForI(' i ')).toBe(true);
-    expect(utils.shouldCapitaliseForI(' i\'')).toBe(true);
-    expect(utils.shouldCapitaliseForI('    i\'')).toBe(true);
-    expect(utils.shouldCapitaliseForI('    i ')).toBe(true);
-    expect(utils.shouldCapitalise('       k')).toBe(false);
-    expect(utils.shouldCapitalise('.       k')).toBe(false);
-    expect(utils.shouldCapitalise('    k ')).toBe(false);
-    expect(utils.shouldCapitalise('    ')).toBe(false);
-});
-
-describe('shouldCapitalise', () => {
-    test('shouldCapitalise_singleLine', () => {
-        expect(utils.shouldCapitalise('war. k')).toBe(true);
-        expect(utils.shouldCapitalise('war.    k')).toBe(true);
-        expect(utils.shouldCapitalise('k')).toBe(true);
-        expect(utils.shouldCapitalise('k')).toBe(true);
-
-        expect(utils.shouldCapitalise('war? k')).toBe(true);
-        expect(utils.shouldCapitalise('war?    k')).toBe(true);
-
-        expect(utils.shouldCapitalise('       k')).toBe(false);
-        expect(utils.shouldCapitalise('.       k')).toBe(false);
-        expect(utils.shouldCapitalise('?       k')).toBe(false);
-        expect(utils.shouldCapitalise('    k ')).toBe(false);
-        expect(utils.shouldCapitalise('    k ')).toBe(false);
-        expect(utils.shouldCapitalise('    ')).toBe(false);
-
-        expect(utils.shouldCapitalise('')).toBe(false);
+describe('utilities test', function() {
+    test('getCapitalisedContent', () => {
+        expect(utils.getCapitalisedContent('blah')).toBe('blaH');
+        expect(utils.getCapitalisedContent('i')).toBe('I');
+        expect(utils.getCapitalisedContent('i\'m')).toBe('i\'M');
+        expect(utils.getCapitalisedContent('i. m')).toBe('i. M');
+        expect(utils.getCapitalisedContent('')).toBe('');
         expect(() => {
-            utils.shouldCapitalise();
+            utils.getCapitalisedContent();
         }).toThrow();
     });
 
-    test('shouldCapitalise_multiLine', () => {
-        expect(utils.shouldCapitalise('war.\n k')).toBe(true);
-        expect(utils.shouldCapitalise('war. \n\n   k')).toBe(true);
-        expect(utils.shouldCapitalise('war. lasting \n peace \n\n   k')).toBe(true);
-        expect(utils.shouldCapitalise('war? \n\n   k')).toBe(true);
-        expect(utils.shouldCapitalise('war? \n\n   k')).toBe(true);
-        expect(utils.shouldCapitalise('war? lasting \n peace \n\n   k')).toBe(true);
+    test('shouldCapitaliseForI', () => {
+        expect(utils.shouldCapitaliseForI('war i ')).toBe(true);
+        expect(utils.shouldCapitaliseForI(' i ')).toBe(true);
+        expect(utils.shouldCapitaliseForI(' i\'')).toBe(true);
+        expect(utils.shouldCapitaliseForI('    i\'')).toBe(true);
+        expect(utils.shouldCapitaliseForI('    i ')).toBe(true);
+        expect(utils.shouldCapitalise('       k')).toBe(false);
+        expect(utils.shouldCapitalise('.       k')).toBe(false);
+        expect(utils.shouldCapitalise('    k ')).toBe(false);
+        expect(utils.shouldCapitalise('    ')).toBe(false);
     });
 
-    test('shouldCapitalise_singleChar', () => {
-        expect(utils.shouldCapitalise('w')).toBe(true);
+    describe('shouldCapitalise', () => {
+        test('shouldCapitalise_singleLine', () => {
+            expect(utils.shouldCapitalise('war. k')).toBe(true);
+            expect(utils.shouldCapitalise('war.    k')).toBe(true);
+            expect(utils.shouldCapitalise('k')).toBe(true);
+            expect(utils.shouldCapitalise('k')).toBe(true);
+
+            expect(utils.shouldCapitalise('war? k')).toBe(true);
+            expect(utils.shouldCapitalise('war?    k')).toBe(true);
+
+            expect(utils.shouldCapitalise('       k')).toBe(false);
+            expect(utils.shouldCapitalise('.       k')).toBe(false);
+            expect(utils.shouldCapitalise('?       k')).toBe(false);
+            expect(utils.shouldCapitalise('    k ')).toBe(false);
+            expect(utils.shouldCapitalise('    k ')).toBe(false);
+            expect(utils.shouldCapitalise('    ')).toBe(false);
+
+            expect(utils.shouldCapitalise('')).toBe(false);
+            expect(() => {
+                utils.shouldCapitalise();
+            }).toThrow();
+        });
+
+        test('shouldCapitalise_multiLine', () => {
+            expect(utils.shouldCapitalise('war.\n k')).toBe(true);
+            expect(utils.shouldCapitalise('war. \n\n   k')).toBe(true);
+            expect(utils.shouldCapitalise('war. lasting \n peace \n\n   k')).toBe(true);
+            expect(utils.shouldCapitalise('war? \n\n   k')).toBe(true);
+            expect(utils.shouldCapitalise('war? \n\n   k')).toBe(true);
+            expect(utils.shouldCapitalise('war? lasting \n peace \n\n   k')).toBe(true);
+        });
+
+        test('shouldCapitalise_singleChar', () => {
+            expect(utils.shouldCapitalise('w')).toBe(true);
+        });
     });
 });
