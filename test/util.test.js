53fd376b1b9ec609561fd7ba6f6db1e37061bd82
Hangjit Rai <rai.hangjit@gmail.com>
2019-08-11 20:26:05 +1000

    ES6 Added Jest Babel Dependencies

--- test/util.test.js
+++ test/util.test.js
@@ -1,7 +1,5 @@
 import * as utils from '../src/utilities.js';
 
-// import * as utils from '~/src/utilities.js';
-
 test('adds 1 + 2 to equal 3', () => {
     expect(utils.getCapitalisedContent('blah')).toBe(3);
 });
