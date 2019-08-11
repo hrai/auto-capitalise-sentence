8fa530a30bc0c51b4ea0554ccb958cc6e59613d8
Hangjit Rai <rai.hangjit@gmail.com>
2019-08-11 19:50:34 +1000

    ES6 Refactored Methods Into Utilities Js File

--- src/content.js
+++ src/content.js
@@ -13,6 +13,12 @@ $(document).ready(function() {
         observeHtmlBody();
     }
 
+    function observeInputTags() {
+        $(':text,textarea').on('input', function(event) {
+            utils.capitaliseText(event.target);
+        });
+    }
+
     function processResponse(item) {
         var sitesToExclude = item.sites_to_ignore;
 
@@ -44,137 +50,6 @@ $(document).ready(function() {
         }
     }
 
-    function onError(error) {
-        console.log(error);
-    }
-
-    function getText(htmlControl, tagName) {
-        if (
-            tagName.toUpperCase() === 'INPUT' ||
-      tagName.toUpperCase() === 'TEXTAREA'
-        ) {
-            return htmlControl.val();
-        }
-
-        return htmlControl.html();
-    }
-
-    function setText(htmlControl, tagName, updatedStr, shouldAppendBr) {
-        if (
-            tagName.toUpperCase() === 'INPUT' ||
-      tagName.toUpperCase() === 'TEXTAREA'
-        ) {
-            htmlControl.val(updatedStr);
-            return;
-        }
-
-        if (shouldAppendBr) {
-            updatedStr += '<br>';
-        }
-
-        htmlControl.html(updatedStr);
-        setEndOfContenteditable(htmlControl[0]);
-    }
-
-    function setEndOfContenteditable(contentEditableElement) {
-        var range, selection;
-        if (document.createRange) {
-            //Firefox, Chrome, Opera, Safari, IE 9+
-            range = document.createRange(); //Create a range (a range is a like the selection but invisible)
-            range.selectNodeContents(contentEditableElement); //Select the entire contents of the element with the range
-            range.collapse(false); //collapse the range to the end point. false means collapse to end rather than the start
-            selection = window.getSelection(); //get the selection object (allows you to change selection)
-            selection.removeAllRanges(); //remove any selections already made
-            selection.addRange(range); //make the range you have just created the visible selection
-        } else if (document.selection) {
-            //IE 8 and lower
-            range = document.body.createTextRange(); //Create a range (a range is a like the selection but invisible)
-            range.moveToElementText(contentEditableElement); //Select the entire contents of the element with the range
-            range.collapse(false); //collapse the range to the end point. false means collapse to end rather than the start
-            range.select(); //Select the range (make it the visible selection
-        }
-    }
-
-    function capitaliseText(element) {
-        if(!element)
-            return;
-
-        var htmlControl = $(element);
-        var tagName = htmlControl.prop('tagName');
-
-        if (!element.isContentEditable && tagName.toUpperCase() !== 'TEXTAREA') {
-            return;
-        }
-
-        var text = getText(htmlControl, tagName);
-
-        //support for jira's comment section's p tags
-        var lastChar = text.trim().slice(-1);
-        if (lastChar.match(/[a-z]/i) && lastChar.toUpperCase() === lastChar) {
-            return;
-        }
-
-        var shouldAppendBr = false;
-        if (text.length >= 4 && text.slice(-4) === '<br>') {
-            text = text.slice(0, -4);
-            shouldAppendBr = true;
-        }
-
-        if (utils.shouldCapitalise(text)) {
-            var updatedStr = getCapitalisedContent(text);
-
-            setText(htmlControl, tagName, updatedStr, shouldAppendBr);
-            return;
-        }
-
-        if (text.length >= 2 && utils.shouldCapitaliseForI(text)) {
-            var updatedStr = getCapitalisedContentForI(text);
-
-            setText(htmlControl, tagName, updatedStr, shouldAppendBr);
-            return;
-        }
-    }
-
-    function getCapitalisedContentForI(text) {
-        var lastTwoChars = text.slice(-2);
-        var updatedStr =
-      text.substr(0, text.length - 2) + lastTwoChars.toUpperCase();
-        return updatedStr;
-    }
-
-    function getCapitalisedContent(text) {
-        var lastChar = text.slice(-1);
-        var updatedStr = text.substr(0, text.length - 1) + lastChar.toUpperCase();
-        return updatedStr;
-    }
-
-    function observeInputTags() {
-        $(':text,textarea').on('input', function(event) {
-            capitaliseText(event.target);
-        });
-    }
-
-    function containsHtmlContent(element) {
-        var content = $(element).html();
-
-        var brRegex = /\s*<br>/;
-        if (content && brRegex.test(content)) return false;
-
-        var regex = /<\/?\w+>/;
-        var hasHtmlTag = regex.test(content);
-        return hasHtmlTag;
-    }
-
-    function isContentEditable(element) {
-        return element && element.isContentEditable;
-    }
-
-    function getFilteredElements(addedNodes, tagName) {
-        return $(addedNodes)
-            .find(tagName)
-            .addBack(tagName); // finds either added alone or as tree
-    }
-
     /*eslint no-debugger: "error"*/
     function observeHtmlBody() {
         var target = document.querySelector('body');
@@ -192,34 +67,34 @@ $(document).ready(function() {
                         // add support for div block in gmail and outlook
                         // if (['P','DIV'].includes(mutation.target.nodeName )) {
                         if (['P'].includes(mutation.target.nodeName)) {
-                            capitaliseText(mutation.target);
+                            utils.capitaliseText(mutation.target);
                             throw new Error(errorMsg);
                         }
 
                         var addedNodes = mutation.addedNodes;
                         if (addedNodes && addedNodes.length > 0) {
                             $.each(tags, function(i, tagName) {
-                                var filteredEls = getFilteredElements(addedNodes, tagName);
+                                var filteredEls = utils.getFilteredElements(addedNodes, tagName);
 
                                 filteredEls.each(function(index, element) {
-                                    if (shouldCapitaliseContent(element)) {
-                                        capitaliseText(element);
+                                    if (utils.shouldCapitaliseContent(element)) {
+                                        utils.capitaliseText(element);
                                     }
                                 });
                             });
 
                             $.each(inputTags, function(i, tagName) {
-                                var filteredEls = getFilteredElements(addedNodes, tagName);
+                                var filteredEls = utils.getFilteredElements(addedNodes, tagName);
 
                                 filteredEls.each(function(index, element) {
                                     $(element).on('input', function(event) {
-                                        capitaliseText(event.target);
+                                        utils.capitaliseText(event.target);
                                     });
                                 });
                             });
                         }
                     } else if (mutation.type === 'characterData') {
-                        capitaliseText(mutation.target.parentNode);
+                        utils.capitaliseText(mutation.target.parentNode);
                     }
                 } catch (err) {
                     if (err.message !== errorMsg) {
@@ -238,8 +113,5 @@ $(document).ready(function() {
         observer.observe(target, config);
     }
 
-    function shouldCapitaliseContent(element) {
-        return isContentEditable(element) && !containsHtmlContent(element);
-    }
 });
 
--- src/utilities.js
+++ src/utilities.js
@@ -21,3 +21,133 @@ export function shouldCapitalise(text) {
     return matches;
 }
 
+export function onError(error) {
+    console.log(error);
+}
+
+
+export function getText(htmlControl, tagName) {
+    if (
+        tagName.toUpperCase() === 'INPUT' ||
+      tagName.toUpperCase() === 'TEXTAREA'
+    ) {
+        return htmlControl.val();
+    }
+
+    return htmlControl.html();
+}
+
+export function setText(htmlControl, tagName, updatedStr, shouldAppendBr) {
+    if (
+        tagName.toUpperCase() === 'INPUT' ||
+      tagName.toUpperCase() === 'TEXTAREA'
+    ) {
+        htmlControl.val(updatedStr);
+        return;
+    }
+
+    if (shouldAppendBr) {
+        updatedStr += '<br>';
+    }
+
+    htmlControl.html(updatedStr);
+    setEndOfContenteditable(htmlControl[0]);
+}
+
+export function setEndOfContenteditable(contentEditableElement) {
+    var range, selection;
+    if (document.createRange) {
+        //Firefox, Chrome, Opera, Safari, IE 9+
+        range = document.createRange(); //Create a range (a range is a like the selection but invisible)
+        range.selectNodeContents(contentEditableElement); //Select the entire contents of the element with the range
+        range.collapse(false); //collapse the range to the end point. false means collapse to end rather than the start
+        selection = window.getSelection(); //get the selection object (allows you to change selection)
+        selection.removeAllRanges(); //remove any selections already made
+        selection.addRange(range); //make the range you have just created the visible selection
+    } else if (document.selection) {
+        //IE 8 and lower
+        range = document.body.createTextRange(); //Create a range (a range is a like the selection but invisible)
+        range.moveToElementText(contentEditableElement); //Select the entire contents of the element with the range
+        range.collapse(false); //collapse the range to the end point. false means collapse to end rather than the start
+        range.select(); //Select the range (make it the visible selection
+    }
+}
+
+
+export function capitaliseText(element) {
+    if(!element)
+        return;
+
+    var htmlControl = $(element);
+    var tagName = htmlControl.prop('tagName');
+
+    if (!element.isContentEditable && tagName.toUpperCase() !== 'TEXTAREA') {
+        return;
+    }
+
+    var text = getText(htmlControl, tagName);
+
+    //support for jira's comment section's p tags
+    var lastChar = text.trim().slice(-1);
+    if (lastChar.match(/[a-z]/i) && lastChar.toUpperCase() === lastChar) {
+        return;
+    }
+
+    var shouldAppendBr = false;
+    if (text.length >= 4 && text.slice(-4) === '<br>') {
+        text = text.slice(0, -4);
+        shouldAppendBr = true;
+    }
+
+    if (utils.shouldCapitalise(text)) {
+        var updatedStr = getCapitalisedContent(text);
+
+        setText(htmlControl, tagName, updatedStr, shouldAppendBr);
+        return;
+    }
+
+    if (text.length >= 2 && utils.shouldCapitaliseForI(text)) {
+        var updatedStr = getCapitalisedContentForI(text);
+
+        setText(htmlControl, tagName, updatedStr, shouldAppendBr);
+        return;
+    }
+}
+
+function getCapitalisedContentForI(text) {
+    var lastTwoChars = text.slice(-2);
+    var updatedStr =
+      text.substr(0, text.length - 2) + lastTwoChars.toUpperCase();
+    return updatedStr;
+}
+
+function getCapitalisedContent(text) {
+    var lastChar = text.slice(-1);
+    var updatedStr = text.substr(0, text.length - 1) + lastChar.toUpperCase();
+    return updatedStr;
+}
+
+function containsHtmlContent(element) {
+    var content = $(element).html();
+
+    var brRegex = /\s*<br>/;
+    if (content && brRegex.test(content)) return false;
+
+    var regex = /<\/?\w+>/;
+    var hasHtmlTag = regex.test(content);
+    return hasHtmlTag;
+}
+
+function isContentEditable(element) {
+    return element && element.isContentEditable;
+}
+
+function getFilteredElements(addedNodes, tagName) {
+    return $(addedNodes)
+        .find(tagName)
+        .addBack(tagName); // finds either added alone or as tree
+}
+
+function shouldCapitaliseContent(element) {
+    return isContentEditable(element) && !containsHtmlContent(element);
+}
