ac86be803bc11bbe88599e706d02fc8e402f61e3
Hangjit Rai <hangjit.rai@outlook.com>
2020-09-22 13:08:26 +1000

    CHROME-SUPPORT CHROME-SUPPORT CHROME-SUPPORT Removed Document.ready

--- src/content.js
+++ src/content.js
@@ -1,125 +1,123 @@
 import * as utils from './utils';
 import browser from 'webextension-polyfill';
 
-$(document).ready(function() {
-  var errorMsg = 'breaking loop';
-  browser.storage.local.get('sites_to_ignore').then(processResponse, utils.onError);
+var errorMsg = 'breaking loop';
+browser.storage.local.get('sites_to_ignore').then(processResponse, utils.onError);
 
-  function hookupEventHandlers() {
-    observeInputTags();
-    observeHtmlBody();
-  }
+function hookupEventHandlers() {
+  observeInputTags();
+  observeHtmlBody();
+}
 
-  function observeInputTags() {
-    $(':text,textarea').on('input', function(event) {
-      capitaliseText(event.target);
-    });
-  }
+function observeInputTags() {
+  $(':text,textarea').on('input', function(event) {
+    capitaliseText(event.target);
+  });
+}
 
-  function processResponse(item) {
-    var sitesToExclude = item.sites_to_ignore;
+function processResponse(item) {
+  var sitesToExclude = item.sites_to_ignore;
 
-    if (item && sitesToExclude) {
-      //https://stackoverflow.com/questions/406192/get-current-url-with-jquery
-      var currentUrlDomain = window.location.origin;
+  if (item && sitesToExclude) {
+    //https://stackoverflow.com/questions/406192/get-current-url-with-jquery
+    var currentUrlDomain = window.location.origin;
 
-      try {
-        var shouldEnableCapitalisingOnCurrentSite = true;
+    try {
+      var shouldEnableCapitalisingOnCurrentSite = true;
 
-        $.each(sitesToExclude, function(_i, siteToExclude) {
-          if (currentUrlDomain.includes(siteToExclude)) {
-            shouldEnableCapitalisingOnCurrentSite = false;
-          }
-        });
-
-        if (shouldEnableCapitalisingOnCurrentSite) {
-          hookupEventHandlers();
-
-          throw new Error(errorMsg);
-        }
-      } catch (e) {
-        if (e.message !== errorMsg) {
-          throw e;
-        }
-      }
-    } else {
-      hookupEventHandlers();
-    }
-  }
-
-  /*eslint no-debugger: "error"*/
-  function observeHtmlBody() {
-    var target = document.querySelector('body');
-
-    // var tags = ['p', 'span', 'div'];
-    var tags = ['p', 'span'];
-    var inputTags = ['input[type=\'text\']', 'textarea'];
-
-    var observer = new MutationObserver(function(mutations) {
-      // console.log(mutations);
-
-      $.each(mutations, function(_i, mutation) {
-        try {
-          if (mutation.type === 'childList') {
-            // add support for div block in gmail and outlook
-            // if (['P','DIV'].includes(mutation.target.nodeName )) {
-            if (['P'].includes(mutation.target.nodeName)) {
-              capitaliseText(mutation.target);
-              throw new Error(errorMsg);
-            }
-
-            var addedNodes = mutation.addedNodes;
-            if (addedNodes && addedNodes.length > 0) {
-              addedNodes.forEach((node) => {
-                if (utils.isFirstTextOfEditableTextNode(node)) {
-                  capitaliseText(node.parentNode);
-                  addedNodes = addedNodes.filter(addedNode=>{addedNode != node;});
-                }
-              });
-
-              $.each(tags, function(_i, tagName) {
-                var filteredEls = utils.getFilteredElements(addedNodes, tagName);
-
-                filteredEls.each(function(_index, element) {
-                  if (utils.shouldCapitaliseContent(element)) {
-                    capitaliseText(element);
-                  }
-                });
-              });
-
-              $.each(inputTags, function(_i, tagName) {
-                var filteredEls = utils.getFilteredElements(addedNodes, tagName);
-
-                filteredEls.each(function(_index, element) {
-                  $(element).on('input', function(event) {
-                    capitaliseText(event.target);
-                  });
-                });
-              });
-            }
-          } else if (mutation.type === 'characterData') {
-            capitaliseText(mutation.target.parentNode);
-          }
-        } catch (err) {
-          if (err.message !== errorMsg) {
-            console.log(err);
-          }
+      $.each(sitesToExclude, function(_i, siteToExclude) {
+        if (currentUrlDomain.includes(siteToExclude)) {
+          shouldEnableCapitalisingOnCurrentSite = false;
         }
       });
+
+      if (shouldEnableCapitalisingOnCurrentSite) {
+        hookupEventHandlers();
+
+        throw new Error(errorMsg);
+      }
+    } catch (e) {
+      if (e.message !== errorMsg) {
+        throw e;
+      }
+    }
+  } else {
+    hookupEventHandlers();
+  }
+}
+
+/*eslint no-debugger: "error"*/
+function observeHtmlBody() {
+  var target = document.querySelector('body');
+
+  // var tags = ['p', 'span', 'div'];
+  var tags = ['p', 'span'];
+  var inputTags = ['input[type=\'text\']', 'textarea'];
+
+  var observer = new MutationObserver(function(mutations) {
+    // console.log(mutations);
+
+    $.each(mutations, function(_i, mutation) {
+      try {
+        if (mutation.type === 'childList') {
+          // add support for div block in gmail and outlook
+          // if (['P','DIV'].includes(mutation.target.nodeName )) {
+          if (['P'].includes(mutation.target.nodeName)) {
+            capitaliseText(mutation.target);
+            throw new Error(errorMsg);
+          }
+
+          var addedNodes = mutation.addedNodes;
+          if (addedNodes && addedNodes.length > 0) {
+            addedNodes.forEach((node) => {
+              if (utils.isFirstTextOfEditableTextNode(node)) {
+                capitaliseText(node.parentNode);
+                addedNodes = addedNodes.filter(addedNode=>{addedNode != node;});
+              }
+            });
+
+            $.each(tags, function(_i, tagName) {
+              var filteredEls = utils.getFilteredElements(addedNodes, tagName);
+
+              filteredEls.each(function(_index, element) {
+                if (utils.shouldCapitaliseContent(element)) {
+                  capitaliseText(element);
+                }
+              });
+            });
+
+            $.each(inputTags, function(_i, tagName) {
+              var filteredEls = utils.getFilteredElements(addedNodes, tagName);
+
+              filteredEls.each(function(_index, element) {
+                $(element).on('input', function(event) {
+                  capitaliseText(event.target);
+                });
+              });
+            });
+          }
+        } else if (mutation.type === 'characterData') {
+          capitaliseText(mutation.target.parentNode);
+        }
+      } catch (err) {
+        if (err.message !== errorMsg) {
+          console.log(err);
+        }
+      }
     });
+  });
 
-    var config = {
-      subtree: true,
-      childList: true,
-      characterData: true
-    };
+  var config = {
+    subtree: true,
+    childList: true,
+    characterData: true
+  };
 
-    observer.observe(target, config);
-  }
+  observer.observe(target, config);
+}
 
-  function capitaliseText(element) {
-    utils.capitaliseText(element, utils.shouldCapitalise, utils.shouldCapitaliseForI,
-      utils.getText , utils.setText);
-  }
-});
+function capitaliseText(element) {
+  utils.capitaliseText(element, utils.shouldCapitalise, utils.shouldCapitaliseForI,
+    utils.getText , utils.setText);
+}
 
