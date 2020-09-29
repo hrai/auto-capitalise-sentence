42faaba8edeca667417c4ee3c189d88a0c5876a5
Hangjit Rai <hangjit.rai@outlook.com>
2020-09-29 18:25:20 +1000

    DISABLE-I Updated Zipper

--- zip_files.sh
+++ zip_files.sh
@@ -7,6 +7,8 @@ fi
 
 npm run build
 
-zip -r9 $PUBLISHED_FILE dependencies icons popup background.js LICENSE manifest.json lib/bundle.js
+zip -r9 $PUBLISHED_FILE dependencies icons popup background.js LICENSE manifest.json lib/bundle.js \
+        node_modules/webextension-polyfill/dist/browser-polyfill.min.js \
+        node_modules/jquery/dist/jquery.min.js
 
 echo "Successfully zipped to '$PUBLISHED_FILE'"
