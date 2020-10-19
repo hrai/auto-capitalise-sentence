94829101e3643efe798306bfc04d11fa7e82d388
hrai <4055444+hrai@users.noreply.github.com>
2020-10-20 09:58:21 +1100

    Capitalise words (#47)

---
+++ src/constants.js
@@ -0,0 +1,26 @@
+const days=[
+  'Monday',
+  'Tuesday',
+  'Wednesday',
+  'Thursday',
+  'Friday',
+  'Saturday',
+  'Sunday'
+];
+
+const months=[
+  'January',
+  'February',
+  'March',
+  'April',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December'
+];
+
+export let constants = days.concat(months);
+
