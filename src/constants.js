82abcde4a5e106be49a7124855bd2aa2a721da5b
Hangjit Rai <hangjit.rai@outlook.com>
2020-10-19 12:58:06 +1100

    CAPITALISE-WORDS Added Months

--- src/constants.js
+++ src/constants.js
@@ -1,9 +1,26 @@
-export let constants = [
+export let constants = days.concat(months);
+
+const days=[
   'Monday',
   'Tuesday',
   'Wednesday',
   'Thursday',
   'Friday',
   'Saturday',
-  'Sunday',
+  'Sunday'
+];
+
+const months=[
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December'
 ];
