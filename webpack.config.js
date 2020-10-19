3451882029e70e04f1590bc970973951ae47c795
Hangjit Rai <hangjit.rai@outlook.com>
2020-10-19 21:05:29 +1100

    REORG-FILES Using Jquery In Webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,23 @@
 const path = require('path');
+const webpack=require( 'webpack');
 
 module.exports = {
-  entry: {app:[ './src/content.js','./src/settings.js',
-    './src/utils.js']},
+
+  entry: {
+    app:[
+      './src/content.js',
+      './src/settings.js',
+      './src/utils.js']
+  },
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'distribution/lib'),
   },
+  // presets: ['@babel/preset-env'],
+  plugins: [
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery'
+    })
+  ]
 };
