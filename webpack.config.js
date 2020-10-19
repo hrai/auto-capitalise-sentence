326ab3570663881ededb3cbaad8a8708064ce0ae
hrai <4055444+hrai@users.noreply.github.com>
2020-10-20 09:50:46 +1100

    Reorg files (#46)

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+const webpack=require( 'webpack');
+
+module.exports = {
+
+  entry: {
+    app:[
+      './src/content.js',
+      './src/settings.js',
+      './src/utils.js']
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'distribution/lib'),
+  },
+  // presets: ['@babel/preset-env'],
+  plugins: [
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery'
+    })
+  ]
+};
