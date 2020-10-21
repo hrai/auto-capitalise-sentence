9c02fc71fe194e858dc21b45ade2b3be48ac0e4b
Hangjit Rai <hangjit.rai@outlook.com>
2020-10-21 17:46:05 +1100

    REMOVE-PKGS Added Eslint-Webpack-Plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,22 @@
 const path = require('path');
-const webpack=require( 'webpack');
+const ESLintPlugin = require('eslint-webpack-plugin');
+const webpack = require('webpack');
 
 module.exports = {
-
   entry: {
-    app:[
-      './src/content.js',
-      './src/settings.js',
-      './src/utils.js']
+    app: ['./src/content.js', './src/settings.js', './src/utils.js'],
   },
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'distribution/lib'),
   },
-  // presets: ['@babel/preset-env'],
   plugins: [
+    new ESLintPlugin({
+      // /*options*/ useEslintrc: true,
+    }),
     new webpack.ProvidePlugin({
       $: 'jquery',
-      jQuery: 'jquery'
-    })
-  ]
+      jQuery: 'jquery',
+    }),
+  ],
 };
