ab980f6e3f6bfc6cd90dd7a70253288b77071ec2
Hangjit Rai <hangjit.rai@outlook.com>
2021-01-15 16:08:34 +1100

    IFRAME Added Rate Limit

--- app.js
+++ app.js
@@ -5,6 +5,19 @@ const port = 3000;
 const path = require('path');
 const router = express.Router();
 
+// set up rate limiter: maximum of five requests per minute
+var RateLimit = require('express-rate-limit');
+var limiter = new RateLimit({
+  windowMs: 1*60*1000, // 1 minute
+  max: 5
+});
+
+// apply rate limiter to all requests
+app.use(limiter);
+
+//add the router
+app.use('/', router);
+
 router.get('/', function(req, res) {
   //__dirname : It will resolve to your project folder.
   res.sendFile(path.join(__dirname + '/index.html'));
@@ -13,9 +26,6 @@ router.get('/', function(req, res) {
 router.get('/iframe', function(req, res) {
   res.sendFile(path.join(__dirname + '/iframe.html'));
 });
-
-//add the router
-app.use('/', router);
 
 app.listen(process.env.port || 3000, () => {
   console.log(`Example app listening at http://localhost:${port}`);
