10d20ca44ed7e8feb590a4a98a5e107bd6f657d0
Hangjit Rai <hangjit.rai@outlook.com>
2022-11-15 10:43:05 +1100

    NEW-WORDS Added New Words

--- src/acronym-constants.js
+++ src/acronym-constants.js
@@ -422,7 +422,7 @@ let awsAcronyms = [
 
 let commonLocalAcronyms = ['Syd', 'Melb', 'AdID', 'AdIDs', 'ABN', 'ACN'];
 
-let localAcronyms = ['GA', 'PE', 'AAv3'];
+let localAcronyms = ['GA', 'PE', 'AAv1', 'AAv3'];
 
 export const acronyms = commonAcronyms.concat(
   timeAcronyms,
