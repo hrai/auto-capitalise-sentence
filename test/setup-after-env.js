7540f9c83da23f0218a7604fca5f8a712e225b3e
Hangjit Rai <hangjit.rai@outlook.com>
2025-10-16 17:31:03 +1100

    cleanup

--- test/setup-after-env.js
+++ test/setup-after-env.js
@@ -1,9 +1,7 @@
 // Global Jest setup after env: ensure timers restored and provide helper resets
 import { __resetAllOptionsAndDictionariesForTests } from '../src/utils';
 
-// Setup jQuery globally (moved from test-env.js to avoid setupFiles issue)
-const $ = require('jquery');
-global.$ = global.jQuery = $;
+// jQuery has been removed from the extension
 
 beforeEach(() => {
   if (typeof __resetAllOptionsAndDictionariesForTests === 'function') {
