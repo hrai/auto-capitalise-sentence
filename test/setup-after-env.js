7e42c120ff65b0f91f05f94332702139ad33fee4
Hangjit Rai <hangjit.rai@outlook.com>
2025-10-17 16:49:29 +1100

    removed all jQuery refs

--- test/setup-after-env.js
+++ test/setup-after-env.js
@@ -1,8 +1,6 @@
 // Global Jest setup after env: ensure timers restored and provide helper resets
 import { __resetAllOptionsAndDictionariesForTests } from '../src/utils';
 
-// jQuery has been removed from the extension
-
 beforeEach(() => {
   if (typeof __resetAllOptionsAndDictionariesForTests === 'function') {
     __resetAllOptionsAndDictionariesForTests();
